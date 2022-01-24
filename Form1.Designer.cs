a73174ff548fd198123ee40580607efc2797ad43
HumN <ahumantr@gmail.com>
2022-01-24 20:43:49 +0300

    firts commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿
+namespace NorthstarModBrowserUpdater
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "Titanfall executable (*.exe)|*.exe";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.progressBar1.Location = new System.Drawing.Point(2, 175);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(406, 38);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(-120, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(651, 55);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Updating";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.label1.UseMnemonic = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(411, 213);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,90 @@
+﻿using Octokit;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+using System.IO.Compression;
+
+namespace NorthstarModBrowserUpdater
+{
+    public partial class Form1 : Form
+    {
+        long gitId= 445768377;
+        string ProgramLocation = "";
+        GitHubClient git;
+        public Form1()
+        {
+
+            InitializeComponent();
+            git = new GitHubClient(new ProductHeaderValue("a"));
+            findTitanfallLocation();
+            downloadUpdate(getReleases().Result.Assets[0].BrowserDownloadUrl);
+        }
+        public void findTitanfallLocation()
+        {
+
+            if (File.Exists("NorthStarModBrowser.config"))
+            {
+                string TitanfallDir = File.ReadAllText("NorthStarModBrowser.config");
+                if (File.Exists(Path.Combine(TitanfallDir, "Titanfall2.exe")))
+                {
+                    ProgramLocation = Path.Combine(TitanfallDir, ProgramLocation, "Norhtstarmodbrowser");
+                    return;
+                }
+
+            }
+            start:
+            openFileDialog1.ShowDialog();
+            if (Path.GetFileName(openFileDialog1.FileName) == "Titanfall2.exe")
+            {
+                ProgramLocation = Path.Combine(Path.GetDirectoryName(openFileDialog1.FileName), ProgramLocation);
+            }
+            else
+            {
+                MessageBox.Show("Selected File wasnt Titanfall2.exe");
+                goto start;
+            }
+            File.WriteAllText("NorthStarModBrowser.config", Path.GetDirectoryName(openFileDialog1.FileName));
+
+        }
+        string installLocation;
+        public async void downloadUpdate(string url)
+        {
+            installLocation = Path.Combine(ProgramLocation, "nsmbUpdate.zip");
+            System.Net.WebClient webClient = new System.Net.WebClient();
+            webClient.DownloadProgressChanged += progressChanged;
+            webClient.DownloadFileCompleted += fileDownloaded;
+            webClient.Headers.Add("user-agent", "Anything");
+            if (System.IO.Directory.Exists(installLocation)) System.IO.Directory.Delete(installLocation, true);
+            await webClient.DownloadFileTaskAsync(new Uri(url), installLocation);
+        }
+        public void fileDownloaded(object sender, AsyncCompletedEventArgs e)
+        {
+            
+            if (System.IO.Directory.Exists(Path.Combine(ProgramLocation, "Norhtstarmodbrowser"))) System.IO.Directory.Delete(Path.Combine(ProgramLocation, "Norhtstarmodbrowser"),true);
+            ZipFile.ExtractToDirectory(installLocation, ProgramLocation);
+            if (File.Exists("NorthStarModBrowser.exe")) File.Delete("NorthStarModBrowser.exe");
+            File.Copy(Path.Combine(ProgramLocation, "Norhtstarmodbrowser", "NorthStarModBrowser.exe"), "NorthStarModBrowser.exe");
+            label1.Text = "Update Complete";
+
+        }
+        public void progressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;
+        }
+        public async Task<Release> getReleases()
+        {
+            // Retrieve a List of Releases in the Repository, and get latest using [0]-subscript
+            var latest = git.Repository.Release.GetAll(gitId).Result[0];
+            return latest;
+        }
+
+    }
+}
